cee017b1289e1a4406d142175c38f6f13576fefd
hilal555 <muhamadhilal04@gmail.com>
2025-01-12 20:50:47 +0700

    membuat fun validasi data

--- app/src/main/java/com/example/firebasefirestore/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebasefirestore/ui/viewmodel/InsertViewModel.kt
@@ -22,6 +22,21 @@ class InserViewModel(
             insertUiEvent = mahasiswaEvent
         )
     }
+
+    fun validateFields(): Boolean{
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenis_kelamin = if (event.jenis_kelamin.isNotEmpty()) null else "Jenis kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong",
+
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
 }
 
 sealed class FormState{
