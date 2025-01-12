5a72665f1e3d138bce195a8208ea279ae981918b
hilal555 <muhamadhilal04@gmail.com>
2025-01-12 18:26:20 +0700

    menambahkan fungsi getMahasiswaByNim

--- app/src/main/java/com/example/firebasefirestore/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/firebasefirestore/repository/NetworkMahasiswaRepository.kt
@@ -3,6 +3,7 @@ package com.example.firebasefirestore.repository
 import com.example.firebasefirestore.model.Mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -59,8 +60,18 @@ private val firestore: FirebaseFirestore
         }
     }
 
-    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
+    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> = callbackFlow {
+        val mhsDocument = firestore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener{ value, error ->
+                if (value != null){
+                    val mhs = value.toObject(Mahasiswa::class.java)!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose{
+            mhsDocument.remove()
+        }
     }
 
 }
