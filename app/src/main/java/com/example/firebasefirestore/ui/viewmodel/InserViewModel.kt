d1bd3d02358d1825260c2b63556e450e31bfb4f8
hilal555 <muhamadhilal04@gmail.com>
2025-01-12 19:35:58 +0700

    membuat data class Mahasiswa Event

--- app/src/main/java/com/example/firebasefirestore/ui/viewmodel/InserViewModel.kt
+++ app/src/main/java/com/example/firebasefirestore/ui/viewmodel/InserViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.firebasefirestore.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.firebasefirestore.repository.MahasiswaRepository
 
@@ -7,4 +10,12 @@ class InserViewModel(
     private val mhs: MahasiswaRepository
 ): ViewModel(){
 
+    data class MahasiswaEvent(
+        val nim: String = "",
+        val nama: String = "",
+        val jenisKelamin: String = "",
+        val alamat: String = "",
+        val kelas: String = "",
+        val angkatan: String = "",
+    )
 }
