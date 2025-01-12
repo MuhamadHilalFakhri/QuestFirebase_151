48d32fbab10fc9182f2e5449958ea2ed4732052d
hilal555 <muhamadhilal04@gmail.com>
2025-01-12 19:39:54 +0700

    membuat fungsi untuk Menyimpan input form ke dalam entity

--- app/src/main/java/com/example/firebasefirestore/ui/viewmodel/InserViewModel.kt
+++ app/src/main/java/com/example/firebasefirestore/ui/viewmodel/InserViewModel.kt
@@ -4,18 +4,32 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.firebasefirestore.model.Mahasiswa
 import com.example.firebasefirestore.repository.MahasiswaRepository
 
 class InserViewModel(
     private val mhs: MahasiswaRepository
 ): ViewModel(){
 
-    data class MahasiswaEvent(
-        val nim: String = "",
-        val nama: String = "",
-        val jenisKelamin: String = "",
-        val alamat: String = "",
-        val kelas: String = "",
-        val angkatan: String = "",
-    )
+
+
 }
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenis_kelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
+
+//Menyimpan input form ke dalam entity
+fun Mahasiswa.toMhsModel():Mahasiswa =Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenis_kelamin = jenis_kelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
